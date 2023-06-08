3ca1d2429ca3bc88fb3e1c7988605f4d48bedcab
mamula2023 <mamulashvili.2017@gmail.com>
2023-06-09 00:35:09 +0400

    reasonable

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,4 +2,26 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
+    public Animal(String color, int numberOfPaws, boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String aboutColor = "This animal is mostly "+this.color+".";
+        String aboutPaws = " It has "+numberOfPaws+" paw";
+        if(this.numberOfPaws != 1)
+            aboutPaws+="s";
+        String aboutFur = " and "+ (hasFur?"a":"no") + " fur.";
+
+        String result = aboutColor + aboutPaws + aboutFur;
+        return result;
+    }
+
+
+
+    private String color;
+    private int numberOfPaws;
+    private boolean  hasFur;
 }
